c8f83b7d113f284ca010162b633813af3b30db39
PC <mattewczlp@seznam.cz>
2025-07-16 11:31:37 +0200

    Fix animation Edu, added interests

--- src/components/About.js
+++ src/components/About.js
@@ -3,23 +3,34 @@ import { useState } from "react";
 function About() {
   const [showEducation, setShowEducation] = useState(false);
   const [showLine, setShowLine] = useState(false);
-  function handleClick() {
+  const [showInt, setShowInt] = useState(false);
+  function handleClickEdu() {
     setShowLine(!showLine);
 
     setTimeout(() => {
       setShowEducation(!showEducation);
-    }, 300);
+    }, 200);
+    setTimeout(() => {
+      setShowLine((prev) => !prev);
+    }, 350);
   }
-
+  function handleClickInt() {
+    setShowInt(!showInt);
+  }
   return (
     <section>
       <h2>About Me</h2>
       <p>This is the about section.</p>
       <div
         className="specifyButton"
-        style={{ position: "relative", display: "inline-block" }}
+        style={{
+          position: "relative",
+          display: "inline-block",
+        }}
       >
-        <h3 onClick={handleClick}>Education</h3>
+        <h3 onClick={handleClickEdu}>Education</h3>
+        <br></br>
+        <h3 onClick={handleClickInt}>Interests</h3>
 
         {showLine && (
           <div className="loading-line">
@@ -29,6 +40,14 @@ function About() {
         {showEducation && (
           <div className="education-info">
             <p>Generic high school</p>
+            <div className="detail-edu">
+              <p>Generic specialization</p>
+            </div>
+          </div>
+        )}
+        {showInt && (
+          <div className="interest-info">
+            <p>Generic interest</p>
           </div>
         )}
       </div>
